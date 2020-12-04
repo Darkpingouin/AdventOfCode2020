f2da3fc63c1ebe5639ed0bdea3bc1b74f7952917
Darkpingouin <aurelie.orset@epitech.eu>
2020-12-04 14:27:11 +0100

    Update d04-part2.py
    Regex end of line + typo on hcl

--- d04/d04-part2.py
+++ d04/d04-part2.py
@@ -11,19 +11,19 @@ def check_line(myStr):
     attributes = myStr.split()
     nbAttributes = 0
     for attribute in attributes:
-        if (is_valid("byr:(19[2-8][0-9]|199[0-9]|200[0-2])", attribute)):
+        if (is_valid("byr:(19[2-8][0-9]|199[0-9]|200[0-2])$", attribute)):
             nbAttributes += 1
-        elif (is_valid("iyr:(201[0-9]|2020)", attribute)):
+        elif (is_valid("iyr:(201[0-9]|2020)$", attribute)):
             nbAttributes += 1
-        elif (is_valid("eyr:(202[0-9]|2030)", attribute)):
+        elif (is_valid("eyr:(202[0-9]|2030)$", attribute)):
             nbAttributes += 1
-        elif (is_valid("(hgt:(1[5-8][0-9]|19[0-3])cm)|(hgt:(59|6[0-9]|7[0-6])in)", attribute)):
+        elif (is_valid("(hgt:(1[5-8][0-9]|19[0-3])cm)|(hgt:(59|6[0-9]|7[0-6])in)$", attribute)):
             nbAttributes += 1
-        elif (is_valid("hcl:#[1-9a-f]{6}",attribute)):
+        elif (is_valid("hcl:#[0-9a-f]{6}$",attribute)):
             nbAttributes += 1 
-        elif (is_valid("ecl:(amb|blu|brn|gry|grn|hzl|oth)", attribute)):
+        elif (is_valid("ecl:(amb|blu|brn|gry|grn|hzl|oth)$", attribute)):
             nbAttributes += 1
-        elif (is_valid("pid:[0-9]{9}", attribute)):
+        elif (is_valid("pid:[0-9]{9}$", attribute)):
             nbAttributes += 1
     return nbAttributes == 7
 
