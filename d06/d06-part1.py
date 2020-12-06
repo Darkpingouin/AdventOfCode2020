160473592ce2bc937c147a63c83a064aba98ae01
Darkpingouin <aurelie.orset@epitech.eu>
2020-12-06 15:48:50 +0100

    d06

---
+++ d06/d06-part1.py
@@ -0,0 +1,22 @@
+import sys
+import string
+
+def check_group(sublist):
+    nbpeople = len(sublist.split())
+    questions = string.ascii_lowercase
+    answers = 0
+    for letter in questions:
+        if (letter in sublist):
+            answers += 1
+    return(answers)
+
+
+def main():
+    mylist = sys.stdin.read().split("\n\n")
+    result = 0
+    for sublist in mylist:
+        result += check_group(sublist)
+    print(result)
+
+if __name__ == "__main__":
+    main()
